c623a252dca9604a07b2374949cc469267f8c1bf
Nacho <ignacio.ortega@opinno.com>
2020-03-16 10:22:50 +0100

    [ADD] Added LocationList Reducer

---
+++ src/store/reducers/locationList/index.tsx
@@ -0,0 +1,48 @@
+import { ActionType, LocationListStateType, LocationListActionType } from '~Store/constants/locationList'
+import { Action } from 'redux'
+
+const initialState: LocationListStateType = {
+  currentPage: 0,
+  error: false,
+  loading: false,
+  name: '',
+  pages: {},
+  totalPages: 0,
+}
+
+export interface DispatchAction extends Action<ActionType> {
+  payload: LocationListActionType;
+}
+
+export const locationListReducer = (
+  state: LocationListStateType = initialState,
+  action: DispatchAction
+): LocationListStateType => {
+  const { type, payload } = action
+  switch (type) {
+    case ActionType.LOCATION_LIST_FAILED:
+      return {
+        ...initialState,
+        error: payload.error,
+        loading: false
+      }
+    case ActionType.LOCATION_LIST_FETCH:
+        return { ...state, loading: true }
+    case ActionType.LOCATION_LIST_SEARCH:
+        return { ...initialState, loading: true }
+    case ActionType.LOCATION_LIST_SUCCEEDED: 
+        return {
+          ...state,
+          currentPage: payload.currentPage,
+          loading: false,
+          name: payload.name,
+          pages: {
+            ...state.pages,
+            [payload.currentPage]: payload.payload.results
+          },
+          totalPages: payload.payload.info.pages
+        }
+    default:
+      return initialState
+  }
+}

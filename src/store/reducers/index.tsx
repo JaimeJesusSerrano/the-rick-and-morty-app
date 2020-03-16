753f993b659ebfe95a81def889008fa716940930
Jeff <jeff.uca@gmail.com>
2020-03-16 10:51:19 +0100

    chore: add episode list reducer to combine reducer

--- src/store/reducers/index.tsx
+++ src/store/reducers/index.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { characterListReducer } from '~Store/reducers/characterList'
+import { episodeListReducer} from '~Store/reducers/episodeList/indext'
 
 const rootReducer = combineReducers({
   characterListState: characterListReducer,
+  episodeListState : episodeListReducer,
 })
 
 export default rootReducer

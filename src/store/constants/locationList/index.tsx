a0e54da66f5a21657ba5ec36558fc5fd9939e57d
Nacho <ignacio.ortega@opinno.com>
2020-03-16 10:22:24 +0100

    [ADD] Added LocationList Interfaces

---
+++ src/store/constants/locationList/index.tsx
@@ -0,0 +1,27 @@
+export enum ActionType {
+  LOCATION_LIST_FAILED,
+  LOCATION_LIST_FETCH,
+  LOCATION_LIST_SEARCH,
+  LOCATION_LIST_SUCCEEDED,
+}
+
+type Dictionary<T> = { [index: number]: T | undefined }
+// type Dictionary = Record<number, {}>
+
+export interface LocationListStateType {
+  currentPage?: number
+  error?: boolean
+  loading?: boolean
+  name?: string
+  pages: Dictionary<number|undefined>
+  totalPages: number
+}
+
+export interface LocationListActionType {
+  currentPage: number
+  error?: boolean
+  name: string
+  pages: {}
+  payload?: any
+  type: string
+}

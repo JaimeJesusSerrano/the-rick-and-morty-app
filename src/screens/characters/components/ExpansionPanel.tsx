597cf533f26c3f122cfea8b9a706b16ea64d401a
Jeff <jeff.uca@gmail.com>
2020-03-17 12:47:59 +0100

    chore: mod dropzone to 2 divs
    possible containers of cards

--- src/screens/characters/components/ExpansionPanel.tsx
+++ src/screens/characters/components/ExpansionPanel.tsx
@@ -3,23 +3,28 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import { ExpansionPanelDetails } from '@material-ui/core'
-import SearchContainer from './SearchBar'
+import { ExpansionPanelDetails, ExpansionPanelActions } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
 
 const ExpansionPanelComparator = (): JSX.Element => {
   return (
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography> Comparator</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>DROP ZONE</Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+    <ExpansionPanel>
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography> Comparator</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <div>
+          <Typography> Character one</Typography>
+        </div>
+        <div>
+          <Typography> Character two</Typography>
+        </div>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   )
 }
 

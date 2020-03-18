00d5feb0529d876d2125fbf9db2a913c0fca011a
Jeff <jeff.uca@gmail.com>
2020-03-18 09:44:23 +0100

    chore: add card-action/basic-structure to make a card selectable

--- src/screens/characters/components/Card.tsx
+++ src/screens/characters/components/Card.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Card as MaterialUiCard, CardContent } from '@material-ui/core'
+import {
+  Card as MaterialUiCard,
+  CardContent,
+  CardActionArea,
+  CardActions,
+  CardMedia,
+} from '@material-ui/core'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import styled from 'styled-components'
+import Button from '@material-ui/core/Button'
 import { Character } from '../../../services/api/types'
 import CardContentItem from '~Screens/characters/components/CardContentItem'
 import UnknownIcon from '~Assets/img/unknown.jpeg'
@@ -29,23 +36,34 @@ const Card = ({
 }: CardProps) => {
   return (
     <StyledCard>
-      <LazyLoadImage
-        alt={name}
-        effect="blur"
-        height={200}
-        placeholderSrc={UnknownIcon}
-        src={image}
-        threshold={500}
-        width={200}
-      />
-      <StyledCardContent>
-        <CardContentItem title="NAME" value={name} />
-        <CardContentItem title="STATUS" value={status} />
-        <CardContentItem title="SPICES" value={species} />
-        <CardContentItem title="GENDER" value={gender} />
-        <CardContentItem title="ORIGIN" value={origin.name} />
-        <CardContentItem title="LAST LOCATION" value={location.name} />
-      </StyledCardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          Selected
+        </Button>
+      </CardActions>
+      <CardActionArea
+        onClick={()=>(console.log(`Selected: ${name}`))}
+      >
+        <CardMedia>
+          <LazyLoadImage
+            alt={name}
+            effect="blur"
+            height={200}
+            placeholderSrc={UnknownIcon}
+            src={image}
+            threshold={500}
+            width={200}
+          />
+        </CardMedia>
+        <StyledCardContent>
+          <CardContentItem title="NAME" value={name} />
+          <CardContentItem title="STATUS" value={status} />
+          <CardContentItem title="SPICES" value={species} />
+          <CardContentItem title="GENDER" value={gender} />
+          <CardContentItem title="ORIGIN" value={origin.name} />
+          <CardContentItem title="LAST LOCATION" value={location.name} />
+        </StyledCardContent>
+      </CardActionArea>
     </StyledCard>
   )
 }

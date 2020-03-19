6abdfc9d2ca4f22ec00c45d6fb740eb98c17052e
iortega-dev <iortegamancera@gmail.com>
2020-03-19 11:31:22 +0100

    [MOD] Removed unnecesary import

--- src/store/reducers/location/index.tsx
+++ src/store/reducers/location/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { combineReducers } from "redux";
 import { locationListReducer } from "./LocationList";
 import { LocationListStateType } from '~Store/constants/location/LocationList'

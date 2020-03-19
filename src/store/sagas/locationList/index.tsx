d4fd2ec55d833271c3894c8c51d8c3183bf49adf
iortega-dev <iortegamancera@gmail.com>
2020-03-19 14:00:16 +0100

    [MOD] Final refactor locationList saga

--- src/store/sagas/locationList/index.tsx
+++ src/store/sagas/locationList/index.tsx
@@ -1,53 +1,24 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects'
-import { ActionType, DispatchAction, LocationListActionType } from "~Store/constants/location/LocationList";
-import { ApiResponse, get } from '~Api';
+import { ActionType, DispatchAction } from "~Store/constants/location/LocationList";
+import { get } from '~Api';
 
 // REFACTOR FUNCTION
-function* getLocationsApi(payload : LocationListActionType) {
-  try {
-    const { data }: ApiResponse<'location', {}> = yield call(
-            get,
-            'location',
-            payload
-          )
-    return data
-  } catch (error) { // Controls no internet connection
-    yield put({ criticalError: true, type: ActionType.LOCATION_LIST_CONNECTION_FAILED}) // Stops loading and show no response
-    throw Error(ActionType.LOCATION_LIST_CONNECTION_FAILED) // Throw error for tracing
-  }
-}
-
 function* getLocations({payload} : DispatchAction) {
   const { page: currentPage, name } = payload
-  const data = yield call(getLocationsApi, payload)
-  if (data)
+  try {
+    const { data } = yield call(get, 'location',payload)
     yield put({
       payload: data, currentPage, name,
       type: ActionType.LOCATION_LIST_SUCCEEDED
     })
-  else 
-    yield put({ error: true, type: ActionType.LOCATION_LIST_FAILED})
+  } catch (error) {
+    if (error.response) { // Response but no results
+      yield put({ error: true, type: ActionType.LOCATION_LIST_FAILED})
+    } else { // No response from server
+      yield put({ criticalError: true, type: ActionType.LOCATION_LIST_CONNECTION_FAILED})
+    }
+  }
 }
-
-// OLD FUNCTION
-// function* getLocations({ payload }: DispatchAction) {
-//   const { page: currentPage, name } = payload
-//   try {
-//     const { data }: ApiResponse<'location', {}> = yield call(
-//       get,
-//       'location',
-//       payload
-//     )
-
-//     yield put({
-//       payload: data, currentPage, name,
-//       type: ActionType.LOCATION_LIST_SUCCEEDED
-//     })
-
-//   } catch (e) {
-//     yield put({ error: true, type: ActionType.LOCATION_LIST_FAILED})
-//   }
-// }
 
 function* locationsSaga() {
   yield all([

c7579a4c3b82aee00175f88cb925e0aef06d9d2a
Jeff <jeff.uca@gmail.com>
2020-03-26 13:01:40 +0100

    chore: add send character action

--- src/store/actions/character/Comparator.ts
+++ src/store/actions/character/Comparator.ts
@@ -1,19 +1,11 @@
 import { ActionType } from '~Store/constants/character/Comparator'
-import {CardProps} from '~Screens/characters/components/Card'
+import { Character } from '~Api/types'
 
-export const sendCharacterSelected = (character: CardProps) => {
+export const sendCharacterSelected = (character: Character) => {
   return {
-    type: ActionType.COMPARATOR_CHARACTER_SELECTED_POST,
+    type: ActionType.COMPARATOR_CHARACTER_SELECTED_PUT_SUCCESS,
     payload: {
       character,
-    },
-  }
-}
-
-export const fetchCharacterSelected = () => {
-  return {
-    type: ActionType.COMPARATOR_CHARACTER_SELECTED_POST,
-    payload: {
     },
   }
 }

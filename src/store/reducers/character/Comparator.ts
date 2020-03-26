e622e3f68d73078d5d38484c6d3c419dcd66a231
Jeff <jeff.uca@gmail.com>
2020-03-26 13:03:23 +0100

    chore: add send character reducer

---
+++ src/store/reducers/character/Comparator.ts
@@ -0,0 +1,32 @@
+import {
+  ActionType,
+  RequestType,
+  StateType,
+} from '~Store/constants/character/Comparator'
+
+const initialState: StateType = {
+  error: false,
+  charactersSelected: [],
+}
+
+export const comparatorReducer = (
+  state: StateType = initialState,
+  action: RequestType
+): StateType => {
+  switch (action.type) {
+    case ActionType.COMPARATOR_CHARACTER_SELECTED_PUT_FAILED:
+      return {
+        ...initialState,
+        error: action.error,
+      }
+
+    case ActionType.COMPARATOR_CHARACTER_SELECTED_PUT_SUCCESS:
+      return {
+        ...state,
+        charactersSelected: [...state.charactersSelected, action.payload]
+
+      }
+    default:
+      return initialState
+  }
+}
--- src/store/reducers/character/index.tsx
+++ src/store/reducers/character/index.tsx
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux'
 import { listReducer } from './List'
 import { StateType as CharacterListStateType } from '~Store/constants/character/List'
+import { comparatorReducer } from './Comparator'
+import { StateType as ComparatorStateType } from '~Store/constants/character/Comparator'
 
 interface CharacterType {
   list: CharacterListStateType
+  comparator: ComparatorStateType
 }
 
 export const characterReducer = combineReducers<CharacterType>({
   list: listReducer,
+  comparator: comparatorReducer,
 })

724c4d3fcf05e9e706b57c00b769bbfa5fb2c6d9
Jeff <jeff.uca@gmail.com>
2020-03-27 12:49:38 +0100

    styled: del comments and code structure

--- src/store/constants/character/Comparator.ts
+++ src/store/constants/character/Comparator.ts
@@ -1,17 +1,9 @@
 import { Character } from '~Api/types'
 
-
 export enum ActionType {
   COMPARATOR_CHARACTER_SELECTED_PUT_FAILED = 'COMPARATOR_CHARACTER_SELECTED_PUT_FAILED',
   COMPARATOR_CHARACTER_SELECTED_PUT_SUCCESS = 'COMPARATOR_CHARACTER_SELECTED_PUT',
 }
-
-
-// type CardPropsState = CardProps[] | [CardProps] | [CardProps, CardProps]
-//
-// const testProps : CardProps[] = []
-//
-// const cardTest : CardPropsState = testProps
 
 export interface StateType {
   error?: boolean

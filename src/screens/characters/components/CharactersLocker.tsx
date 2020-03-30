7cfb7f17cefaf257ae48edf34297d64c7f2e0b81
Jeff <jeff.uca@gmail.com>
2020-03-30 08:54:45 +0200

    chore: add deselect dispatch action

--- src/screens/characters/components/CharactersLocker.tsx
+++ src/screens/characters/components/CharactersLocker.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { v4 as uuidv4 } from 'uuid'
 import { CardActionArea, CardMedia, Container } from '@material-ui/core'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
@@ -7,6 +7,7 @@ import { CharacterComparableInfo } from './Card'
 import UnknownIcon from '~Assets/img/unknown.jpeg'
 import { RootState } from '~Store/reducers'
 import { Character } from '~Api/types'
+import { deleteCharacterSelected } from '~Store/actions/character/Comparator'
 
 type CharactersSelected =
   | [CharacterComparableInfo, CharacterComparableInfo]
@@ -34,10 +35,16 @@ const CharactersLocker = (): JSX.Element => {
   const fetchCharacterSelected = useSelector(
     (state: RootState) => state.character.comparator
   )
+  const dispatch = useDispatch()
+
   const charactersSelected = fetchCharacterSelected.charactersSelected.map(
     (characterSelected: Character) => (
       <div key={uuidv4()}>
-        <CardActionArea>
+        <CardActionArea
+          onClick={() => {
+            dispatch(deleteCharacterSelected(characterSelected.id))
+          }}
+        >
           <CardMedia>
             <LazyLoadImage
               alt={characterSelected.name}

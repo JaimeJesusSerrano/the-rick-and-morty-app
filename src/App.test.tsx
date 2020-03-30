a8401a7efa40440ee11995d59566d08f7d583a8f
JaimeJesúsSerrano <jaimejesusserrano@gmail.com>
2020-03-30 09:45:01 +0200

    feat: Add selenium-webdriver config and Home example

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { shallow } from 'enzyme'
+import { By, Key, until } from 'selenium-webdriver'
 import { configureStore } from '~Store/config'
 import App from './App'
+import { seleniumDriver } from './setupTests'
 
 const store = configureStore()
 
@@ -16,3 +18,14 @@ test('[App] Provider is loaded correctly', () => {
   const ProviderComponent = <Provider store={store} />
   AppComponent.contains([ProviderComponent])
 })
+
+test('[App] Load correctly', async () => {
+  try {
+    await seleniumDriver.get('http://localhost:3000/')
+    await seleniumDriver.wait(until.titleIs('React App'), 10000)
+    await seleniumDriver.get('http://localhost:3000/characters')
+    await seleniumDriver.wait(until.titleIs('React App'), 10000)
+  } finally {
+    await seleniumDriver.quit()
+  }
+}, 30000)
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,11 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import webdriver from 'selenium-webdriver'
 
 configure({ adapter: new Adapter() })
+
+export const seleniumDriver = new webdriver.Builder()
+  .forBrowser('chrome')
+  // .setChromeOptions({})
+  // .setFirefoxOptions({})
+  .build()

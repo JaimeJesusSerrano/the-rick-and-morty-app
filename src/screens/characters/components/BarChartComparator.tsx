5f52c488f9711455430d24c2979fe4f9a09447b4
Jeff <jeff.uca@gmail.com>
2020-03-31 11:18:03 +0200

    chore: add data values to chart comparator

--- src/screens/characters/components/BarChartComparator.tsx
+++ src/screens/characters/components/BarChartComparator.tsx
@@ -6,34 +6,38 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend, ResponsiveContainer,
+  Legend,
 } from 'recharts'
 import { Container } from '@material-ui/core'
 
-const BarChartComparator = (): JSX.Element => {
+type DataValues = {
+  name: string
+  similarity: number
+}
 
-  const data = [
-    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-  ];
+export type DataComparator = {
+  characterCompared: string
+  data: DataValues[]
+}
+
+
+interface BarChartComparatorProps {
+ values: DataComparator
+}
+
+const BarChartComparator = ({values}:BarChartComparatorProps): JSX.Element => {
 
   return (
       <Container>
-      <BarChart width={600} height={300} data={data}
+      <BarChart width={600} height={300} data={values.data}
                 margin={{top: 5, right: 30, left: 20, bottom: 5}}
       >
         <CartesianGrid strokeDasharray="3 3"/>
         <XAxis dataKey="name"/>
-        <YAxis/>
+        <YAxis />
         <Tooltip/>
-        <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
+        <Legend  />
+        <Bar dataKey="similarity" fill="#82ca9d" name="Level Similarity"/>
       </ BarChart>
       </Container>
   )

51c826b0c4e30a2d43eccf6b107631e9cd7288c5
Jeff <jeff.uca@gmail.com>
2020-03-31 12:19:22 +0200

    style: prettier format barchart component

--- src/screens/characters/components/BarChartComparator.tsx
+++ src/screens/characters/components/BarChartComparator.tsx
@@ -21,34 +21,33 @@ export type DataComparator = {
   data: DataValues[]
 }
 
-
 interface BarChartComparatorProps {
- values: DataComparator
+  values: DataComparator
 }
 
-const BarChartComparator = ({values}:BarChartComparatorProps): JSX.Element => {
-
+const BarChartComparator = ({
+  values,
+}: BarChartComparatorProps): JSX.Element => {
   return (
-      <Container>
-      <BarChart width={600} height={300} data={values.data}
-                margin={{top: 5, right: 30, left: 20, bottom: 5}}
+    <Container>
+      <BarChart
+        width={600}
+        height={300}
+        data={values.data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
       >
-        <CartesianGrid strokeDasharray="3 3"/>
-        <XAxis dataKey="name"/>
-        <YAxis domain={[0,4]}>
-        <Label
-          angle={270}
-          position="left"
-          style={{textAnchor: 'middle'}}
-        >
-          Maximum Similarity(4)
-        </Label>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis domain={[0, 4]}>
+          <Label angle={270} position="left" style={{ textAnchor: 'middle' }}>
+            Maximum Similarity(4)
+          </Label>
         </YAxis>
-        <Tooltip/>
-        <Legend  />
-        <Bar dataKey="similarity" fill="#82ca9d" name="Similarity Level"/>
-      </ BarChart>
-      </Container>
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="similarity" fill="#82ca9d" name="Similarity Level" />
+      </BarChart>
+    </Container>
   )
 }
 

2442f8f73e49d9fcf1256ecd656a042777f9f89b
iortega-dev <iortegamancera@gmail.com>
2020-04-02 17:17:14 +0200

    [ADD] Added idle sidebar implementation

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,11 @@
 import { Card, CardContent, CardMedia, Typography } from '@material-ui/core'
 import React from 'react'
 import styled from 'styled-components'
+import { useIdle } from "@react-corekit/use-idle";
+
+// Images import
+import EvilMortyImg from '~Assets/img/evilmorty.jpeg'
+import GoodMortyImg from '~Assets/img/goodmorty.jpeg'
 
 const StyledCard = styled(Card)`
   /* background-color: ${({ theme }) => theme.palette.primary.light}; */
@@ -9,21 +14,20 @@ const StyledCard = styled(Card)`
 `
 
 const Sidebar = () => {
+
+  const options = { timeToIdle: 3000 };
+  const isIdle = useIdle(options);
+
   return (
     <>
-      <h1>Sidebar</h1>
-      <h3>Rick & Morty random elements</h3>
       <StyledCard>
         <CardMedia
-          style={{ height: 140 }}
-          image="https://cdn.themedizine.com/2020/01/cuarta_temporada_rick_morty-1000x489.jpeg"
+          style={{ height: 250, transition: 'all .5s ease-in-out' }}
+          image={isIdle ? EvilMortyImg : GoodMortyImg}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Title
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Data here
+            {isIdle ? "...or maybe not." : "Such a good boy..."}
           </Typography>
         </CardContent>
       </StyledCard>

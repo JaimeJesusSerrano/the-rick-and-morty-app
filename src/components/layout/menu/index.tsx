51817de5c2a51b8ad4588f55e8a0f9988e56d677
iortega-dev <iortegamancera@gmail.com>
2020-04-02 09:43:42 +0200

    [ADD] Added responsive menu implementation

--- src/components/layout/menu/index.tsx
+++ src/components/layout/menu/index.tsx
@@ -1,11 +1,9 @@
-import { AppBar, Grid, Toolbar } from '@material-ui/core'
+import { AppBar, Grid, Toolbar, Hidden } from '@material-ui/core'
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 import Logo from '~Components/Logo'
-import routes from '~Routes/routes'
-import Search from './Search'
-import Section from './Section'
+import NavbarMenu from '~Components/layout/NavbarMenu'
+import NavbarMobile from '~Components/layout/NavbarMobile'
 
 const StyledAppBar = styled(AppBar)`
   background-color: ${({ theme }) => theme.palette.secondary.main};
@@ -16,41 +14,20 @@ const StyledToolbar = styled(Toolbar)`
 `
 
 const Menu = () => {
-  const location = useLocation()
   return (
     <StyledAppBar position="static">
       <StyledToolbar>
         <Grid container direction="row" justify="center" alignItems="center">
-          <Grid item>
+          <Grid item xs={1}>
             <Logo />
           </Grid>
-          <Grid item xs={6} />
-          <Grid id="menu-screen-characters" item xs>
-            <Section
-              path={location.pathname}
-              text="Characters"
-              to={routes.characters.path}
-              variant="h6"
-            />
-          </Grid>
-          <Grid id="menu-screen-episodes" item xs>
-            <Section
-              path={location.pathname}
-              text="Episodes"
-              to={routes.episodes.path}
-              variant="h6"
-            />
-          </Grid>
-          <Grid id="menu-screen-locations" item xs>
-            <Section
-              path={location.pathname}
-              text="Locations"
-              to={routes.locations.path}
-              variant="h6"
-            />
-          </Grid>
-          <Grid item xs={2}>
-            <Search />
+          <Grid item xs={11} container direction="row" justify="flex-end">
+            <Hidden smDown>
+              <NavbarMenu />
+            </Hidden>
+            <Hidden mdUp>
+              <NavbarMobile />
+            </Hidden>
           </Grid>
         </Grid>
       </StyledToolbar>
